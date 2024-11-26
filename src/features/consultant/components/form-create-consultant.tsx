1d5a126a3bdb1bf7654c322496cda6db48fd7009
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-11-26 08:34:47 +0100

    render error

--- src/features/consultant/components/form-create-consultant.tsx
+++ src/features/consultant/components/form-create-consultant.tsx
@@ -2,17 +2,19 @@
 
 import { useActionState } from "react";
 import { Button, Input } from "@/components";
+import { createConsultant } from ".";
 
 export function FormCreateConsultant() {
   const initialState = { message: null, errors: {} };
   const [state, formAction] = useActionState(createConsultant, initialState);
 
   return (
-    <form >
+    <form action={formAction}>
       <Input typeOfInput="text" label="First Name" nameOfInput="firstName" />
       <Input typeOfInput="text" label="Last Name" nameOfInput="LastName" />
       <Input typeOfInput="email" label="Email" nameOfInput="email" />
       <Button label="Save" type="submit" />
+      {state ? <pre>{JSON.stringify(state?.errors)}</pre> : ""}
     </form>
   );
 }
