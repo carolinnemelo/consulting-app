01c443e4da29e968de39813acf879d951efb79da
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-11-26 11:53:34 +0100

    remove comment

--- src/db/types.ts
+++ src/db/types.ts
@@ -1,5 +1,5 @@
 import { consultantsTable } from ".";
 
 export type ConsultantInsert = typeof consultantsTable.$inferInsert;
-export type ConsultantUpdate = Partial<ConsultantInsert> & {id: number}; // partial makes all the values optional and inside the object mandatory
+export type ConsultantUpdate = Partial<ConsultantInsert> & {id: number};
 
