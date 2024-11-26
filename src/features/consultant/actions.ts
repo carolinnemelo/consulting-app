bdf371378de409a804482d2be93d70fa82518c8d
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-11-26 12:50:04 +0100

    remove log

--- src/features/consultant/actions.ts
+++ src/features/consultant/actions.ts
@@ -4,7 +4,6 @@ import { revalidatePath } from "next/cache";
 import { z } from "zod";
 import { consultantFeature } from ".";
 
-
 const createConsultantSchema = z.object({
   firstName: z.string().trim().min(1, "Can not be empty").trim(),
   lastName: z.string().trim().min(1, "Can not be empty"),
@@ -26,18 +25,19 @@ export async function createConsultant(prevState: State, formData: FormData) {
     lastName: formData.get("lastName"),
     email: formData.get("email"),
   });
+
   if (!validatedFields.success) {
     return {
       errors: validatedFields.error.flatten().fieldErrors,
       message: "Missing Fields",
     };
   }
+
   try {
     const newConsultant = validatedFields.data;
     consultantFeature.service.create(newConsultant);
   } catch (error) {
     return { success: false, message: error.message };
   }
-  console.log(validatedFields);
   revalidatePath("/my-profile");
 }
