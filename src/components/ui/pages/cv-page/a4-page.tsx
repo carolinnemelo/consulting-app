820d7c98a1323d62762db9fbfa0fd2997dc41b4b
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-11-28 21:33:45 +0100

    remove rule from global css

--- src/components/ui/pages/cv-page/a4-page.tsx
+++ src/components/ui/pages/cv-page/a4-page.tsx
@@ -12,7 +12,7 @@ export async function A4Page() {
   const consultantData = await consultantFeature.service.getCV(name);
   return (
     <div className="">
-      <div className="grid grid-cols-1 md:mx-auto md:grid-cols-4  md:min-h-[1122px] md:max-h-[1122px]  md:min-w-[795px] md:max-w-[795px] bg-white shadow-lg shadow-zinc-400">
+      <div className="grid grid-cols-1 md:mx-auto md:grid-cols-4  md:min-h-[1122px] md:max-h-[1122px]  md:min-w-[795px] md:max-w-[795px] bg-white shadow-lg shadow-zinc-300">
         <div className="text-primary bg-muted h-[50px] text-xl pr-4 flex items-center justify-end md:col-start-1 md:col-end-5 md:row-start-1">
           &lt;/salt&gt;
         </div>
@@ -40,36 +40,4 @@ export async function A4Page() {
       </div>
     </div>
   );
-}
-
-{
-  /* <div className="a4-page">
-<div className="grid grid-cols-4 grid-rows-12 h-full gap-3">
-  <div className="text-primary bg-muted col-start-1 col-end-5 row-start-1 h-[50px] text-xl pr-4 flex items-center justify-end">
-    &lt;/salt&gt;
-  </div>
-  <aside className="col-start-1 row-start-1 row-end-13 pt-16 h-full text-start px-4 bg-muted space-y-3">
-    <Image
-      src={`/${name}.png`}
-      height={170}
-      width={170}
-      alt="Logo"
-      className="justify-self-center"
-    />
-    <h2 className="text-sm font-bold uppercase">Technical Skills</h2>
-    <AsideTitles consultantData={consultantData} />
-  </aside>
-  <article className="col-start-2 col-span-3 pr-6 space-y-3 -mt-6">
-    <ArticleGeneralInfo consultantData={consultantData} />
-    <h4 className="text-base font-extrabold uppercase">
-      Education & Training
-    </h4>
-    <ArticleEducation consultantData={consultantData} />
-    <h4 className="text-base font-extrabold uppercase">
-      Experience
-    </h4>
-    <ArticleExperience consultantData={consultantData} />
-  </article>
-</div>
-</div> */
 }
