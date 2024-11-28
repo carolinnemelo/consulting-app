b2256ef1849f20b2e6d7ee9c767bf18f39d63fd2
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-11-28 15:13:06 +0100

    add logic layer

--- src/db/types.ts
+++ src/db/types.ts
@@ -1,4 +1,7 @@
-import { consultantsTable } from ".";
+import { consultantsEducationTable, consultantsSkillsTable, consultantsTable, consultantsWorkTable } from ".";
 
 export type ConsultantInsert = typeof consultantsTable.$inferInsert;
+export type ConsultantSkillsInsert = typeof consultantsSkillsTable.$inferInsert;
+export type ConsultantEducationInsert = typeof consultantsEducationTable.$inferInsert;
+export type ConsultantWorkInsert = typeof consultantsWorkTable.$inferInsert;
 export type ConsultantUpdate = Omit<ConsultantInsert, "id" | "createdAt">;
--- src/features/consultant/service.ts
+++ src/features/consultant/service.ts
@@ -4,6 +4,7 @@ import { date } from "drizzle-orm/mysql-core";
 
 export function createService() {
   return {
+    
     async getAll() {
       return await db.select().from(consultantsTable);
     },
@@ -37,6 +38,10 @@ export function createService() {
       if (existingConsultant) {
         throw new Error("Consultant already exist. Please, try another email.");
       }
+      let consultantGeneral;
+      let consultantSkills;
+      let consultantEducations;
+      
       // const { firstName, lastName, email, bio, backendItems, generalItems } = consultant;
       await db.insert(consultantsTable).values({ ...consultant });
     },
@@ -47,5 +52,24 @@ export function createService() {
         .set({ updatedAt: new Date(), ...consultant })
         .where(eq(consultantsTable.id, id))
     },
+
+
+    async validateFields() {
+      createConsultantSchema.safeParse({
+        firstName: formData.get("firstName"),
+        lastName: formData.get("lastName"),
+        email: formData.get("email"),
+        bio: formData.get("bio"),
+        generalItems: formData.get("generalItems"),
+        backendItems: formData.get("backendItems"), 
+        frontendItems: formData.get("frontendItems"), 
+        toolsItems:  formData.get("toolsItems"),
+        socialItems:  formData.get("socialItems"),
+        school:  formData.get("school"),
+        degree:  formData.get("degree"),
+        year: formData.get("year"),
+        details: formData.get("details"),
+      });
+    }
   };
 }
